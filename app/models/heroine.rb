526cd97d853fa84c910b7b79459e0f094eb50d36
David Tenenbaum <github email address>
2018-06-15 10:34:28 -0400

    NAILED IT

--- app/models/heroine.rb
+++ app/models/heroine.rb
@@ -1,2 +1,27 @@
 class Heroine < ApplicationRecord
+  belongs_to :power
+
+  validates :super_name, uniqueness: true
+
+  def search=(search)
+    @search = search
+  end
+
+  def search
+    @search
+  end
+
+  def self.search(search)
+    if search
+      superpower = Power.find_by(name: search)
+      if superpower
+        self.where(power_id: superpower)
+      else
+        Heroine.all
+      end
+    else
+      Heroine.all
+    end
+  end
+
 end
